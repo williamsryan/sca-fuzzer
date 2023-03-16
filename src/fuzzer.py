297bd1bd3c9930e89faa4fe095e7d49ff25648ae
Ryan Williams <ryan.rpwilliams@gmail.com>
2023-03-16 19:08:47 -0400

    Tests

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -165,6 +165,7 @@ class Fuzzer:
 
         for id, input in enumerate(inputs):
             run = self.model.execute(input)
+            print(f"[+] Run debug: {run}")
             # print(f"[+] Run obj: {run}")
             run.id = id
             # self.store_run(run)
