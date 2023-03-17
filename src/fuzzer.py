586da88b94a89354f5fd21ef209bc8dc9f76a8c5
Ryan Williams <williams.ry@northeastern.edu>
2023-03-17 00:31:34 -0400

    asdf

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -80,8 +80,8 @@ class Fuzzer:
         # create all main modules
         self.initialize_modules()
 
-        run1: Run
-        run2: Run
+        run1: Run()
+        run2: Run()
 
         for i in range(num_test_cases):
             LOGGER.fuzzer_start_round(i)
