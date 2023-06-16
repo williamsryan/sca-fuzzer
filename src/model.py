ed8c2436933374b42d65c3a9884881b3ed1ac6f2
Ryan Williams <ryan.rpwilliams@gmail.com>
2023-06-16 16:07:42 -0400

    hm

--- src/model.py
+++ src/model.py
@@ -432,7 +432,8 @@ class UnicornModel(Model, ABC):
                                                      model.code_start]
             dbg_instr = model.test_case.address_map[address - model.code_start]
 
-            print(f"[model] Instr name: {dbg_instr.name} Instr op: {dbg_instr.get_mem_operands()}")
+            # print(f"[model] Instr name: {dbg_instr.name} Instr op: {dbg_instr.get_mem_operands()}")
+            print(f"[model] Instr name: {dbg_instr.name} Instr op: {dbg_instr.operands}")
 
             if (model.current_instruction.is_instrumentation):
                 instr += " #instrumentation"
