3933a5929eab8f79df49eb63764126db8d742de8
Ryan Williams <ryan.rpwilliams@gmail.com>
2023-06-19 09:12:58 -0400

    oops

--- src/cli.py
+++ src/cli.py
@@ -264,9 +264,8 @@ def main() -> int:
                 # the register values from those instructions (e.g., a violation is input-dependent).
                 # TODO: test for differences in memory instruction operands/addresses.
                 #       A difference in addresses for same instruction would be a new clause.
-                for archstate in run1.archstates:
-                    for mems in archstate:
-                        print(f"[+] Mme test: {mems}")
+                for xstate in run1.archstates:
+                    print(f"[+] xstate test: {xstate}")
                 
                 synth = Synthesizer(theory_fname, args.working_directory, 1)
                 # Each run object corresponds to an execution of a same program with different inputs
