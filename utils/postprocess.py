68930ae515f6da40cd9b5b58edf4d2e749f2de85
Ryan Williams <ryan.rpwilliams@gmail.com>
2023-06-21 10:57:10 -0400

    Added postprocessor stub

---
+++ utils/postprocess.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+    TODO: this will be a class for parsing the learned contracts.
+    We want to map the integer values of registers to x86 names.
+    There may be other name resolution things we want to do later.
+"""
+
+import sys
+
+if __name__ == "__main__":
+    sys.exit(0)
