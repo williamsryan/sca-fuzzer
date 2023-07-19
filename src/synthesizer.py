16c52012405e502ba264e38db0bc3afac6875f48
Ryan Williams <ryan.rpwilliams@gmail.com>
2023-07-19 10:14:38 -0400

    new commenting

--- src/synthesizer.py
+++ src/synthesizer.py
@@ -57,13 +57,13 @@ class Synthesizer:
             # guide the synthesizer to learning instructions vs. registers.
             for instr in instrs:
                 print(f"[synthesizer] Test instrs: {instr}: ")
-                comment_obj = f"; Register state for instruction: {instr}"
+                regs += f" ; Register state for instruction: {instr}"
                 # for op in instr.operands:
                 #     print(f"[synthesizer]\toperands: {op.value}")
                 #     comment_obj += f" operands: {op.value}"
 
             # Run step registers object.
-            return "\n{0}\n(define {1} (list {2}))\n\n".format(comment_obj, xstate_name, regs)
+            return f"\n(define {xstate_name} (list {regs}))\n"
 
         with open(self.work_dir + "/" + self.filename, "a") as f:
             xstates = ''
