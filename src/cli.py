2e29620c37507c60db1729018275175bf8b9c0ba
Ryan Williams <ryan.rpwilliams@gmail.com>
2023-07-20 11:18:16 -0400

    updating archstates

--- src/cli.py
+++ src/cli.py
@@ -311,8 +311,8 @@ def main() -> int:
                 synth = Synthesizer(theory_fname, args.working_directory, 1)
                 # Each run object corresponds to an execution of a same program with different inputs
                 # that produce the same contract trace and different hardware trace.
-                synth.map_regs(run1)
-                synth.map_regs(run2)
+                synth.map(run1)
+                synth.map(run2)
                 # TODO: implement the instructions into run object.
                 synth.generate_constraints(pairs, run1, run2)
 
